dd17f734751dcf9aa007fcddd326ba86464a09c2
ovaw <kei99dr@gmail.com>
2021-10-22 05:29:36 +0900

    ビューポート変換とresize関数の実装 まだ画像はゆがむけどウィンドウサイズを取得できた

--- Sample/Sample.cpp
+++ Sample/Sample.cpp
@@ -1,4 +1,5 @@
 ﻿#include <gl/freeglut.h>
+#include <bits/stdc++.h>
 
 #define X 0
 #define Y 1
@@ -7,25 +8,28 @@
 unsigned int num_points = 5;
 double point[][3] = { {1.3, 1.3, 0.0}, {0.3, 1.3, 0.0}, {0.3, 0.3, 0.0}, {1.3, 0.3, 0.0}, {0.8, 0.8, 0.0} };
 
+unsigned int window_width, window_height; // ウインドウサイズ用大域変数
+
 void display(void)
 {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(-2.0, 2.0, -2.0, 2.0, -100.0, 100.0);
+	glViewport(0, 0, window_width, window_height); // 投影によって得られた画像をウィンドウにはめ込む
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);          // これから描く図形のタイプ：三角形の描画
+	glBegin(GL_TRIANGLES);                         // これから描く図形のタイプ：三角形の描画
 
-	glColor3f(1.0f, 0.0f, 0.0f);    // 着色 赤
-	glVertex3dv(point[0]);          // glVertex3dv() 座標配列のポインタで指定
+	glColor3f(1.0f, 0.0f, 0.0f);  // 着色 赤
+	glVertex3dv(point[0]);        // glVertex3dv() 座標配列のポインタで指定
 	glVertex3dv(point[1]);
 	glVertex3dv(point[4]);
 
-	glColor3f(0.0f, 1.0f, 0.0f);    // 緑
+	glColor3f(0.0f, 1.0f, 0.0f);  // 緑
 	glVertex3dv(point[1]);
 	glVertex3dv(point[2]);
 	glVertex3dv(point[4]);
 
-	glColor3f(0.0f, 0.0f, 1.0f);    // 青
+	glColor3f(0.0f, 0.0f, 1.0f);  // 青
 	glVertex3dv(point[2]);
 	glVertex3dv(point[3]);
 	glVertex3dv(point[4]);
@@ -39,6 +43,13 @@ void initGL(void)
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // ウインドウの色(RGB), α値(透明度) 黒
 }
 
+void resize(int width, int height) // 新しいサイズを取得し記録する
+{
+	printf("Size %d x %d\n", width, height);
+	window_width  = width;
+	window_height = height;
+}
+
 int main(int argc, char* argv[])
 {
 	glutInitWindowPosition(128, 128); // ウインドウ位置指定（左上隅）
@@ -47,6 +58,7 @@ int main(int argc, char* argv[])
 	glutInitDisplayMode(GLUT_RGBA);   // 表示に用いるフレームバッファの設定 GLUT_RGBA:ビットごとの論理和 GLUT_DEPTH:デプスバッファ処理に対応
 	glutCreateWindow(argv[0]);        // OpenGLによる表示用ウインドウの生成 引数は名前
 	glutDisplayFunc(display);         // 表示用コールバック関数の設定 
+	glutReshapeFunc(resize);
 	initGL();
 	glutMainLoop();                   // イベントの発生を待ち続けるループ関数の一種 ユーザの強制終了まで待機し続ける
 
